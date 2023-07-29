b8c8197e704d83eafc317a7ea64a95e86020e8fc
KeithMurph <keithmurphy94@gmail.com>
2023-07-29 02:03:27 -0700

    new port

---
+++ src/content_option.js
@@ -0,0 +1,110 @@
+const logotext = "Keith";
+const meta = {
+    title: "Keith Murphy",
+    description: "My name is Keith Murphy, I am a full stack developer from Seattle Washington.",
+};
+
+const introdata = {
+    title: "I’m Keith Murphy",
+    animated: {
+        first: "Full Stack Web Developer",
+        second: "I like making games",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+
+
+const skills = [{
+        name: "JavaScript",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+
+const dataportfolio = [{
+        img: "https://i.postimg.cc/dV4nsphQ/Screenshot-2022-04-08-034350.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.postimg.cc/qvKMV29S/smartcam-Screen-04-11-092934.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.postimg.cc/NMKZ2J0F/Screenshot-2021-08-22-204634.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.postimg.cc/BvRTt043/Screenshot-2022-04-08-030622.jpg",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.postimg.cc/ydprVFXK/Screenshot-2021-08-24-234915.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://i.postimg.cc/NfWdkFt9/Screenshot-2022-02-04-102616.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://i.postimg.cc/hj0Qxr3F/Screenshot-2021-08-26-005304.png",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "keithmurphy.dev@gmail.com",
+    YOUR_FONE: "(520)891-2082",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/KeithMurph",
+    linkedin: "https://www.linkedin.com/in/keithmurph/",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
